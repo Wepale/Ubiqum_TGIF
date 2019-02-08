a01081f440dec918edd8ac9f79ee3b08d7cb1d55
Unknown <wepale17@gmail.com>
2019-02-08 18:44:49 +0100

    Filters and some style done
    Filters, accordion effect on home page and some style on checkbox

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 /*
- *Create a new tbody element
+ *Create a new tbody tag
  *@param tBodyID the id attribute of this new tbody
  *@param tableID the id attribute of the table we want to append this new tr
  */
@@ -10,7 +10,7 @@ const createTbody = (tBodyID, tableID) => {
 }
 
 /*
- *Create a new thead element
+ *Create a new thead tag
  *@param tHeadID the id attribute of this new thead
  *@param elementID the id attribute of the table we want to append this new thead
  */
@@ -22,7 +22,7 @@ const createThead = (tHeadID, elementID) => {
 }
 
 /*
- *Create a new tr element
+ *Create a new tr tag
  *@param trID the id attribute of this new tr
  *@param elementID the id attribute of the table we want to append this new tr
  */
@@ -34,7 +34,7 @@ const createTr = (trID, elementID) => {
 }
 
 /*
- *Create a new th element
+ *Create a new th tag
  *@param info the info inside this th (<th>info</th>)
  *@param trID the id attribute of the tr we want to append this new th
  */
@@ -46,7 +46,7 @@ const createTh = (info, trID) => {
 }
 
 /*
- *Create a new td element
+ *Create a new td tag
  *@param info the info inside this td (<td>info</td>)
  *@param trID the id attribute of the tr we want to append this new td
  */
@@ -57,7 +57,14 @@ const createTd = (info, trID) => {
   document.getElementById(trID).appendChild(newTd);
 }
 
-const createTdwithLink = (info, link, trID) => {
+/*
+ *Create a new td tag with a link inside
+ *@param info the text inside the a (<a href="#">info</a>)
+ *@param link the ur inside the href atributte (<a href="link"><a>)
+ *@param trID the id attribute of the tr we want to append this new td
+ */
+
+const createTdWithLink = (info, link, trID) => {
   //Create Link (<a href="Link">info</a>)
   let newLink = document.createElement("a");
   newLink.setAttribute('href', link);
@@ -67,16 +74,162 @@ const createTdwithLink = (info, link, trID) => {
   let newTd = document.createElement("TD");
   newTd.appendChild(newLink);
   document.getElementById(trID).appendChild(newTd);
-
 }
 
-//Array with all Senate Members
-let senateMembersList = senateData.results[0].members;
-// //Array with all House of Representative Members
-// let houseMembersList = houseData.results[0].members;
+/*
+ *Create a new option tag
+ *@param info the text inside the option and the vakue attribute (<option value="info">info</option>)
+ *@param id the id attribute of the section tag we want to append this option
+ */
+
+const createOption = (info, id) => {
+  let newOption = document.createElement('option');
+  newOption.setAttribute("value", info);
+  newOption.innerHTML = info;
+  document.getElementById(id).appendChild(newOption);
+}
+
+/*
+ *Fill the dropdown menu of <select> whit <option> that contains the states of the memebers
+ *@param membersArran array with all the members from the JSON
+ *@param id the id attribute of the element we want to append the options
+ */
+const createStateSelection = (membersArr, id) => {
+  const states = [];
+  for (member of membersArr) {
+    if (!states.includes(member.state)) {
+      states.push(member.state);
+    }
+  }
+  for (state of states.sort()) {
+    createOption(state, id);
+  }
+}
+
+/*
+ * Create the table
+ *
+ *@param membersArr an array with all the members from the JSON
+ *@param tableTitleArr an array with the titles of our table
+ *@param info an array with all the keys from the JSON we want to show. The strings inside the array must mach
+ *       the keys of the JSON letter by letter.
+ */
+
+const createTable = (membersArr, tableTitlesArr, keysArr) => {
+  createThead("myHead", "senate_data");
+  createTbody("myBody", "senate_data");
+  createTr("trTitle", "myHead");
+  for (title of tableTitlesArr) {
+    createTh(title, "trTitle");
+  }
+  let dynamicID = 0;
+  let modkeysArr = keysArr.filter(data => data !== "middle_name" && data !== "last_name");
+  for (member of membersArr) {
+    //Create the names of the variables dynamically. let is not possible because of scope problems
+    eval(`var {${keysArr.join(", ")}} = member`);
+    let id = `member${++dynamicID}`;
+    createTr(id, "myBody");
+    //Concatenate the names without null values
+    const fullNameArray = keysArr.filter(name => eval(name) !== null && (name === "first_name" || name === "middle_name" || name === "last_name"));
+    if (fullNameArray.length) {
+      let completeName = "";
+      for (name of fullNameArray) {
+        completeName = `${completeName} ${eval(name)}`;
+      }
+      first_name = completeName;
+    }
+    //Add % on "votes_with_party_pct"
+    if (modkeysArr.includes("votes_with_party_pct")) {
+      votes_with_party_pct += "%";
+    }
+    for (variable of modkeysArr) {
+      if (variable === "first_name") {
+        createTdWithLink(eval(variable), member.url, id);
+      } else {
+        createTd(eval(variable), id);
+      }
+    }
+  }
+}
+
+/*
+ *Filter the members by partys
+ *@param membersArr an array with all the members from the JSON
+ *@param partyArr an array whit the partys we want to filter
+ *@return an array thah contains the memers filter by party
+ */
+
+const filterByParty = (membersArr, partyArr) => {
+  if (partyArr.length) {
+    let filterArr = [];
+    for (party of partyArr) {
+      filterArr.push(...membersArr.filter(member => member.party === party));
+    }
+    return filterArr;
+  }
+  return membersArr;
+}
+
+/*
+ *Filter the members by state
+ *@param membersArr an array with all the members from the JSON
+ *@param state a string whit the state we want to filter
+ *@return an array thah contains the memers filter by state
+ */
+
+const filterByState = (membersArr, state) => {
+  return state === "all" ? membersArr : membersArr.filter(member => member.state === state);
+}
+/*
+ * Delete the existin
+ *
+ *
+ *
+ */
+const changeTable = (membersArr, tableTitlesArr, keysArr, tableID) => {
+  let table = document.getElementById(tableID)
+  while (table.hasChildNodes()) {
+    table.removeChild(table.firstChild);
+  }
+  createTable(membersArr, tableTitlesArr, keysArr);
+}
+
+const filterAll = (membersArr) => {
+  //Filter by Party
+  let checkArr = Array.from(document.getElementsByName("party"));
+  let helpArr = [];
+  for (check of checkArr) {
+    if (check.checked) helpArr.push(check.value);
+  }
+  helpArr = filterByParty(membersArr, helpArr);
+  //Filter By State
+  let opS = document.getElementById("state");
+  helpArr = filterByState(helpArr, opS.options[opS.selectedIndex].value);
+  //Delete table and create the new one
+  changeTable(helpArr, tableTitles2, arrayInfo2, "senate_data")
+}
+
+const makeAccordion = () => {
+  let myButton = document.getElementById("accordion");
+  myButton.classList.toggle("active");
+  let panel = document.getElementById("panel");
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = `${panel.scrollHeight}px`;
+  }
+}
+
+
+
+
+
+
+
+/////////////Execute JS///////////////
 
 /****************************************************************************
-                    MODIFY THIS ARRAYS FOR CHANGE THE TABLE
+                    MODIFY THIS ARRAYS TO CHANGE THE TABLE
 
 The position of the elements in arrayInfo must be relative to the position of
 elements of tableTitles
@@ -102,51 +255,10 @@ const tableTitles3 = ["Party", "Office", "Years in Oficce", "% Votes w/ Party", 
 //Array with our Keys from JSON
 const arrayInfo3 = ["party", "office", "seniority", "votes_with_party_pct", "last_updated"];
 
-/*
- * Create the table
- *
- *@param membersArr an array with all the members from the JSON
- *@param tableTitleArr an array with the titles of our table
- *@param info an array with all the keys from the JSON we want to show. The strings inside the array must mach
- *       the keys of the JSON letter by letter.
- */
 
-const createTable = (membersArr, tableTitlesArr, keysArr) => {
-  createThead("myHead", "senate_data");
-  createTbody("myBody", "senate_data");
-  createTr("trTitle", "myHead");
-  for (let title of tableTitlesArr) {
-    createTh(title, "trTitle");
-  }
-  let dynamicID = 0;
-  let modkeysArr = keysArr.filter(data => data !== "middle_name" && data !== "last_name");
-  for (member of membersArr) {
-    //Create the names of the variables dynamically. let is not possible because of scope problems
-    eval(`var {${keysArr.join(", ")}} = member`);
-    let id = `member${++dynamicID}`;
-    createTr(id, "myBody");
-    //Concatenate the names without null values
-    const fullNameArray = keysArr.filter(name => eval(name) !== null && (name === "first_name" || name === "middle_name" || name === "last_name"));
-    if (fullNameArray.length) {
-      let completeName = "";
-      for (name of fullNameArray) {
-        completeName += eval(name) + " ";
-      }
-      first_name = completeName.trim();
-    }
-    //Add % on "votes_with_party_pct"
-    if (modkeysArr.includes("votes_with_party_pct")) {
-      votes_with_party_pct += "%";
-    }
-    //Fill table with links on names
-    for (variable of modkeysArr) {
-      // createTd(eval(variable), id);
-      if (variable === "first_name") {
-        createTdwithLink(eval(variable), member.url, id);
-      } else {
-        createTd(eval(variable), id);
-      }
-    }
-  }
-}
-createTable(senateMembersList, tableTitles2, arrayInfo2);
+//Array with all Senate Members
+let memberList = senateData.results[0].members;
+//Fill the dropdown menu whit all the state on the JSON
+createStateSelection(memberList, "state");
+//Create the inital table
+createTable(memberList, tableTitles2, arrayInfo2);
